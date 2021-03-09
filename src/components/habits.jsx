6f551f40cef3faf022f172c76911babbc95acb3f
juyong <juyong0822@gmail.com>
2021-03-09 16:41:09 +0900

    [Fix] Add key in habits list

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -5,9 +5,9 @@ class Habits extends Component {
 
     state = {
         habits: [
-            {name: 'Reading', count: 0},
-            {name: 'Running', count: 0},
-            {name: 'Coding', count: 0},
+            {id: 1, name: 'Reading', count: 0},
+            {id: 2, name: 'Running', count: 0},
+            {id: 3, name: 'Coding', count: 0},
         ]
     };
 
@@ -17,7 +17,7 @@ class Habits extends Component {
                 {
                     this.state.habits.map(habit => {
                         return (
-                            <Habit habit={habit}/>
+                            <Habit key={habit.id} habit={habit}/>
                         );
                     })
                 }
