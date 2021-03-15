d678395b8e9ad83976684e3fd271601b75e52965
juyong <juyong0822@gmail.com>
2021-03-15 15:22:27 +0900

    [Refactor] Delete callback functions in habits component

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,20 +2,6 @@ import React, { Component } from "react";
 import Habit from "./habit";
 
 class Habits extends Component {
-    // make callback functions
-    // if button clicked, just call the functions
-    handleIncrement = (habit) => {
-        this.props.onIncrement(habit);
-    };
-
-    handleDecrement = (habit) => {
-        this.props.onDecrement(habit);
-    };
-
-    handleDelete = (habit) => {
-        this.props.onDelete(habit);
-    };
-
     render() {
         return (
             <ul>
@@ -24,9 +10,9 @@ class Habits extends Component {
                         <Habit
                             key={habit.id}
                             habit={habit}
-                            onIncrement={this.handleIncrement}
-                            onDecrement={this.handleDecrement}
-                            onDelete={this.handleDelete}
+                            onIncrement={this.props.onIncrement}
+                            onDecrement={this.props.onDecrement}
+                            onDelete={this.props.onDelete}
                         />
                     );
                 })}
