ced3574c0055e88017c302cf8780e6441c14ec80
juyong <juyong0822@gmail.com>
2021-03-17 18:41:14 +0900

    [New Feature] Remove reset component

--- src/components/reset.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Reset extends Component {
-    render() {
-        return <button onClick={this.props.onReset}>Reset All</button>;
-    }
-}
-
-export default Reset;
